88d274552dc360109f042edd2ccb40c409256243
Unisbern <unisbern@gmail.com>
2023-01-06 00:07:59 +0300

    Создана форма для заполнения поля виджета с навыками
    Баг: при нажатии кнопки отмена в виджет добавляется пустая строка

--- skillsform.cpp
+++ skillsform.cpp
@@ -12,3 +12,39 @@ SkillsForm::~SkillsForm()
 {
     delete ui;
 }
+
+void SkillsForm::on_pushButton_clicked()
+{
+    ui->listWidget->addItem(createDialog());
+
+}
+
+QString SkillsForm::createDialog()
+{
+    qDebug()<<__FUNCTION__;
+    QDialog dlg(this);
+    dlg.setFixedSize(300,90);
+    dlg.setWindowTitle(tr("Введите навык"));
+
+    QLineEdit *ledit1 = new QLineEdit(&dlg);
+
+    QDialogButtonBox *btn_box = new QDialogButtonBox(&dlg);
+    btn_box->setStandardButtons(QDialogButtonBox::Ok | QDialogButtonBox::Cancel);
+
+    connect(btn_box, &QDialogButtonBox::accepted, &dlg, &QDialog::accept);
+    connect(btn_box, &QDialogButtonBox::rejected, &dlg, &QDialog::reject);
+
+    QFormLayout *layout = new QFormLayout();
+    layout->addRow(tr("Введите навык"), ledit1);
+    layout->addWidget(btn_box);
+    dlg.setLayout(layout);
+
+    if(dlg.exec() == QDialog::Accepted) {
+        const QString &str1 = ledit1->text();
+        return ledit1->text();
+    }
+    else if (dlg.exec() == QDialog::Rejected){
+        return {};
+    }
+}
+
--- skillsform.h
+++ skillsform.h
@@ -2,6 +2,11 @@
 #define SKILLSFORM_H
 
 #include <QWidget>
+#include <QDialog>
+#include <QDebug>
+#include <QLineEdit>
+#include <QDialogButtonBox>
+#include <QFormLayout>
 
 namespace Ui {
 class SkillsForm;
@@ -15,8 +20,12 @@ public:
     explicit SkillsForm(QWidget *parent = nullptr);
     ~SkillsForm();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::SkillsForm *ui;
+    QString createDialog();
 };
 
 #endif // SKILLSFORM_H
